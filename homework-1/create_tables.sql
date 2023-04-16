6b01b33f0f07c798aba6c58ff904f8e2d3bd5032
samea <sameanoniM@gmail.com>
2023-04-16 18:52:40 +0300

    new file:   .vscode/settings.json modified:   homework-1/create_tables.sql modified:   homework-1/main.py

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    title VARCHAR(100),
+    birth_date DATE,
+    notes TEXT
+);
+
+CREATE TABLE customers(
+    customers_id int PRIMARY KEY,
+    company_name VARCHAR(100) NOT NULL,
+    contact_name text
+);
+
+CREATE TABLE orders (
+  order_id INT PRIMARY KEY,
+  customers_id INT,
+  employee_id INT,
+  order_date DATE DEFAULT CURRENT_DATE,
+  ship_city VARCHAR(50),
+  CONSTRAINT fk_customers FOREIGN KEY (customers_id) REFERENCES customers (customers_id),
+  CONSTRAINT fk_employees FOREIGN KEY (employee_id) REFERENCES employees (employee_id)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,34 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+# Подключение к базе данных
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="postgres"
+)
+
+# Создание курсора
+cur = conn.cursor()
+
+# Открытие файла CSV
+with open('./orders_data.csv', 'r') as f:
+    # Пропуск заголовка
+    next(f)
+    # Копирование данных из файла в таблицу orders
+    cur.copy_from(f, 'orders', sep=',')
+
+with open('./customers_data.csv', 'r') as f:
+    next(f)
+    cur.copy_from(f, 'customers', sep=',')
+
+with open('./employees_data.csv', 'r') as f:
+    cur.copy_from(f, 'employees', sep=',')
+
+# Фиксация транзакции
+conn.commit()
+
+# Закрытие соединения
+conn.close()
